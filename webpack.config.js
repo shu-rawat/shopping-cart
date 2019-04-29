11d4f8aafe29746b78ddc5bf8e82d462c1902781
shu-rawat <shubham.rawat@publicissapient.com>
2019-04-29 17:20:07 +0530

    c

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+module.exports = {
+    entry: './main.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module:{
+        rules:[
+                {
+                    test:/\.(s*)css$/,
+                    use:['style-loader','css-loader', 'sass-loader']
+                }
+         ]
+    }
+};
